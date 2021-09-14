ca8ccd4ac7cbe99a3397eda9666a1838c8b2483b
Brendon Henrique <brendonhps@gmail.com>
2021-09-13 23:55:01 -0300

    add gin framework in the project

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,35 @@ package main
 
 import (
 	"github.com/brendontj/lol-stats/app"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
-	baseURI := "https://esports-api.lolesports.com/persisted/gw/" //Todo Add env vars
-	token := "0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z"           //Todo Add env vars
-	baseURIFeed := "https://feed.lolesports.com/livestats/v1/"    //Todo Add env vars
+	initRoutes()
+}
 
-	application := app.NewApplication(baseURI, token, baseURIFeed)
-	application.Start()
-	application.PopulateHistoricalData()
-	application.Close()
+func initRoutes() {
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		 c.Data(http.StatusOK, "text/html", []byte("pong"))
+	})
+
+	router.GET("/sync_data", func(c *gin.Context) {
+		baseURI := "https://esports-api.lolesports.com/persisted/gw/" //Todo Add env vars
+		token := "0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z"           //Todo Add env vars
+		baseURIFeed := "https://feed.lolesports.com/livestats/v1/"    //Todo Add env vars
+
+		application := app.NewApplication(baseURI, token, baseURIFeed)
+		application.Start()
+		application.PopulateHistoricalData()
+		application.Close()
+
+		c.Data(http.StatusOK, "text/html", []byte("synced"))
+	})
+
+	if err := router.Run(); err != nil {
+		panic(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,18 @@ module github.com/brendontj/lol-stats
 go 1.14
 
 require (
+	github.com/gin-gonic/gin v1.7.4
+	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/jackc/pgx/v4 v4.11.0
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/pkg/errors v0.8.1
 	github.com/satori/go.uuid v1.2.0
+	github.com/ugorji/go v1.2.6 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/sys v0.0.0-20210910150752-751e447fb3d0 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
